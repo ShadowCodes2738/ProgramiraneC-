664f9b6ba9753761356f8b70fc84feeb14590201
ShadowCodes2738 <99739409+ShadowCodes2738@users.noreply.github.com>
2022-09-26 09:43:51 +0300

    Add files via upload

---
+++ Realen tip/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Realen_tip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            char bukva = 'C';
+            byte cifra = 100;
+            short number = 20000;
+            int num = 300000;
+            string specialnost = "Programirane";
+            Console.WriteLine("Promenlivata ot char e = " + bukva);
+            Console.WriteLine("Byte e  = " + cifra);
+            Console.WriteLine("Shorta e = " + number);
+            Console.WriteLine("Inta e = " + num);
+            Console.WriteLine("Stringa e = " + specialnost);
+
+        }
+    }
+}
