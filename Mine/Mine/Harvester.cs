f74741afc9c6216a5909215d9c7fa171a8bed87b
ShadowCodes2738 <99739409+ShadowCodes2738@users.noreply.github.com>
2022-09-26 11:27:55 +0300

    Add files via upload

---
+++ Mine/Mine/Harvester.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mine
+{
+    class Harvester
+    {
+        private string id;
+        private double oreOutput;
+        private double energyRequirement;
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public double OreOutput
+        {
+            get { return oreOutput; }
+            set { oreOutput = value; }
+        }
+
+        public double EnergyRequirment
+        {
+            get { return energyRequirement; }
+            set { if (value <= 0 || value > 20000) 
+            { throw new ArgumentException("Enegry must be between 0 and 20000"); }
+            else
+            { energyRequirement = value; }
+        }
+    }
+
+        public Harvester(string id,double oreOutput,double energyRequriment )
+        {
+            this.Id = id;
+            this.OreOutput = oreOutput;
+            this.EnergyRequirment = energyRequirement;
+        }
+    }
+}
---
+++ Transport/Transport/Truck.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transport
+{
+    class Truck
+    {
+        private string name;
+        private double capacity;
+        private double weight;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("Name cannot be empty");
+                name = value;
+        }
+
+        public double Capacity
+        {
+            get { return capacity; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Name cannot be empty");
+                capacity = value;
+            }
+
+        }
+        public double Weight
+        {
+            get { return weight; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Name cannot be empty");
+                weight = value;
+
+            }
+        }
+        public Truck(string name, double capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.Weight = 0;
+        }
+        public bool canLoading(double weight)
+        {
+            if (capacity >= this.weight + weight)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void Loading(double weight)
+        {
+            this.weight += weight;
+        }
+       
+
+        }
+    }
+            }
+
