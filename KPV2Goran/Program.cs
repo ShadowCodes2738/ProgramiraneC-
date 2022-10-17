c6ab62b0304a8760514a75d3c78d51500710ae61
ShadowCodes2738 <99739409+ShadowCodes2738@users.noreply.github.com>
2022-10-17 09:43:20 +0300

    Add files via upload

---
+++ KPV2Goran/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace KPV2Goran
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vuvedete dve dvuichni chisla:");
+            string chislo1 = Console.ReadLine();
+            string chislo2 = Console.ReadLine();
+            Console.Write("Vuvedete znak:");
+            string znak = Console.ReadLine();
+           int chislo11 = Convert.ToInt16(chislo1);
+           int chislo22 = Convert.ToInt16(chislo2); 
+           int znak1 = Convert.ToInt16(znak);
+            if (znak == "+")
+            {
+                int sum = chislo11 + chislo22;
+                int sum2 = Convert.ToInt16(sum);
+                Console.WriteLine("Poluchihte=" + sum2);
+            }
+            else if (znak == "-")
+            {
+                int sum = chislo11 - chislo22;
+                int sum2 = Convert.ToInt16(sum);
+                Console.WriteLine("Poluchihte="+sum2);
+            }
+
+             else if (znak == "x")
+            {
+                int sum = chislo11 * chislo22;
+                int sum2 = Convert.ToInt16(sum);
+                Console.WriteLine("Poluchihte=" + sum2);
+
+            }
+            else if (znak == "/")
+            {
+                int sum = chislo11 / chislo22;
+                int sum2 = Convert.ToInt16(sum);
+                Console.WriteLine("Poluchihte=" + sum2);
+
+            }
+        }
+    }
+}
