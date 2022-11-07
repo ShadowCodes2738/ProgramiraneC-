f11cc286c7e2936a00b60923734083fbc50e56d5
ShadowCodes2738 <99739409+ShadowCodes2738@users.noreply.github.com>
2022-11-07 11:35:30 +0200

    Add files via upload

---
+++ Aliqouts/Aliqouts/AliqoutN.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aliqouts
+{
+    class AliqoutN
+    {
+        private int n;
+        private int p;
+
+        public int P
+        {
+            get { return p; }
+            set
+            {
+                if (value % n == 0)
+                {
+
+                    p = value;
+
+                }
+                else
+                {
+                    throw new ArgumentException("p must be a positive number"); ;
+                }
+
+            }
+        }
+        public int N
+        {
+            get { return n; }
+            set
+            {
+                if (value > 0)
+                {
+                    n = value;
+                }
+                else
+                {
+                    throw new ArgumentException("n is not dividable by p");
+                }
+
+            }
+        }
+        public AliqoutN(int n)
+        {
+            this.P = n;
+            this.N = n;
+        }
+        public AliqoutN() : this(1)
+        { }
+        public void SumN(int k)
+        {
+            if (k % n == 0)
+            {
+                p += k;
+            }
+            else
+            {
+                throw new ArgumentException("Argument is not divable by k");
+            }
+        }
+    }
+}   
+//NNN 7.11.2022
+            
+
---
+++ Aliqouts/Aliqouts/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aliqouts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AliqoutN x = new AliqoutN(5);
+            x.SumN(10);
+            Console.WriteLine(x.P);
+            x.SumN(7);
+        }
+    }
+}
