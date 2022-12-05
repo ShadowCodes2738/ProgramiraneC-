b51e85002b4b1c35adec5de30e065b3dbdf5e6af
ShadowCodes2738 <99739409+ShadowCodes2738@users.noreply.github.com>
2022-12-05 09:26:38 +0200

    Add files via upload

---
+++ Broini systemi/Broini systemi/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Broini_systemi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string bin = "101";
+            //int dec = Convert.ToInt32(bin, 2);
+            // Console.WriteLine(dec); 
+           // Console.WriteLine("vuvedete chislo");
+            //string bin = Console.ReadLine();
+           // int dec = Convert.ToInt32(bin, 2);
+           // Console.WriteLine(dec); 
+
+
+
+        }
+    }
+}
---
+++ Broini systemi/Suma na dvoichni chisal/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Suma_na_dvoichni_chisal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bin = int.Parse(Console.ReadLine());
+            int dec = Convert.ToInt32(bin, 2); 
+        }
+    }
+}
